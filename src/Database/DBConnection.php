1686ad7bf869d6b3176d2fe35f4284cc0b17ba03
Valeriy Petrov <piswasser38@gmail.com>
2024-01-11 22:35:14 +0800

    db added

--- src/Database/DBConnection.php
+++ src/Database/DBConnection.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace src\Database;
+namespace Src\Database;
 
 use PDO;
 use PDOException;
@@ -17,28 +17,38 @@ class DBConnection
 
     public PDO $pdo;
 
-    public function __construct($config)
+    public function __construct()
     {
-        $this->driver = $config['driver'];
-        $this->host = $config['host'];
-        $this->dbname = $config['dbname'];
-        $this->login = $config['login'];
-        $this->password = $config['password'];
+        $this->driver = env('DB_DRIVER');
+        $this->dbname = env('DB_NAME') ?? '';
+        $this->dbpath = env('DB_PATH') ? env('DB_PATH') . "{$this->dbname}.sqlite" : '';
+        $this->host = env('DB_HOST');
+        $this->login = env('DB_USERNAME');
+        $this->password = env('DB_PASSWORD');
     }
 
     public function connect(): PDO|string
     {
         try{
-            $this->pdo = new PDO(
-                "$this->driver:host=$this->host;dbname=$this->dbname",
-                $this->login,
-                $this->password,
+            if('sqlite' === strtolower($this->driver)) {
+                $this->pdo = new PDO("{$this->driver}:{$this->dbpath}", '','', [\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC]);
+            }else {
+                $this->pdo = new PDO(
+                    "$this->driver:host=$this->host;dbname=$this->dbname",
+                    $this->login,
+                    $this->password,
 //                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-            );
+                );
+            }
         }catch (PDOException $e) {
               return 'Error: ' . $e->getMessage();
         }
         return $this->pdo;
+    }
+
+    public function getLastId()
+    {
+        return $this->pdo->lastInsertId();
     }
 
 }
--- src/Database/Model.php
+++ src/Database/Model.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
-namespace src\Database;
+namespace Src\Database;
+
+use Src\Database\DBConnection;
 
 class Model
 {
@@ -12,9 +14,34 @@ class Model
         $this->connection = $connection;
     }
 
-    public function query()
+    public function where($field, $data)
     {
-
+        $table = static::$table;
+        $stmt = $this->connection->connect()
+            ->prepare("SELECT * FROM {$table} WHERE {$field} = :data");
+        $stmt->execute([':data' => $data]);
+        return $stmt;
     }
+
+    public function create($data)
+    {
+        $table = static::$table;
+        $values  = str_repeat('?,', count($data) - 1) . '?';
+        $fields = implode(',',array_keys($data));
+        $stmt = $this->connection->connect()
+            ->prepare("INSERT INTO {$table} ({$fields}) VALUES ({$values})");
+        return $stmt->execute(array_values($data));
+    }
+
+    public function getLastId()
+    {
+        return $this->connection->getLastId();
+    }
+
+    public function fetch()
+    {
+        return $this->fetchAll();
+    }
+
 }
 
